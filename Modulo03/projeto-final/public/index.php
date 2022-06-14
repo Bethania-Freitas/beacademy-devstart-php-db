ac2d6dd68ad5a7431399b6c7ab1b7b2deed12cad
= <bethaniafreitas@yahoo.com.br>
2022-06-14 15:42:35 -0300

    aula 10

--- Modulo03/projeto-final/public/index.php
+++ Modulo03/projeto-final/public/index.php
@@ -2,11 +2,9 @@
 
 include '../vendor/autoload.php';
 
-$database = 'db_store';
-$username = 'root';
-$password = 'Bezinha83';
+use App\Connection\Connection;
 
-$connection = new PDO('mysql:host=localhost; dbname=' . $database, $username, $password);
+$connection = Connection::getConnection();
 
 $query = 'SELECT * FROM tb_category;';
 
