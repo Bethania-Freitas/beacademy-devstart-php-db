724057ebd7f2def46a435f61c3f5acb54cd3cc2d
= <bethaniafreitas@yahoo.com.br>
2022-06-14 11:01:42 -0300

    aula01 a 04

---
+++ Modulo03/aula01/db_escola.sql
@@ -0,0 +1,54 @@
+--PARA CRIAR UM BANCO DE DADOS:--
+
+CREATE DATABASE db_escola;
+
+--PARA SELECIONAR O BANCO DE DADOS:--
+
+USE db_escola;
+
+-- CRIANDO TABELA:--
+
+CREATE TABLE tb_professor (
+    nome VARCHAR(100) NOT NULL,
+    cpf CHAR(11) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE tb_aluno (
+    nome VARCHAR(100) NOT NULL,
+    cpf CHAR(11) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    matricula CHAR(20) NOT NULL
+);
+
+--MOSTRANDO TABELAS:--
+
+SHOW TABLES;
+
+
+--INSERIR DADOS NA TABELA;--
+
+INSERT INTO tb_professor(nome, email, cpf)
+VALUES (
+    'Alessandro', 'ale@email.com', '12312312312'
+);
+
+INSERT INTO tb_professor(nome, email, cpf)
+VALUES (
+    'Bruno', 'bruno@email.com', '33311144411'
+);
+
+INSERT INTO tb_aluno(nome, email, cpf, matricula)
+VALUES (
+    'Bethânia', 'bethaniafreitas@yahoo.com.br', '33311122244','127456'
+);
+
+--PARA VER OS DADOS NA TABELA:--
+
+SELECT * FROM tb_professor;
+SELECT * FROM tb_aluno;
+
+--PARA EXCLUIR TABELA--
+
+DROP TABLE tb_professor;
+
