e1fec9f5ff3d76b951ea1665735135daf4a8bab0
= <bethaniafreitas@yahoo.com.br>
2022-06-15 14:26:43 -0300

    aula13

--- Modulo03/projeto-final/src/Controller/CategoryController.php
+++ Modulo03/projeto-final/src/Controller/CategoryController.php
@@ -20,4 +20,25 @@ class CategoryController extends AbstractController
 
         parent::render('category/list', $result);
     }
+
+    public function addAction(): void
+    {
+        if ($_POST) {
+            $name = $_POST['name'];
+            $description = $_POST['description']; 
+
+            $query = "INSERT INTO tb_category (name, description) VALUES('{$name}' ,'{$description}')";
+
+            $con = Connection::getConnection();
+
+            $result = $con->prepare($query);
+            $result->execute();
+
+            echo 'Categoria cadastrada com sucesso!';
+
+
+        }
+
+        parent::render('category/add');
+    }
 }
